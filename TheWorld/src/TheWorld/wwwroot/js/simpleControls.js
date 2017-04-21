db65c7dcdef821bfc3c336f4812abbd462b8bd12
Mark Anderson <markanderson2001@live.com>
2017-04-21 02:00:52 -0500

    bu042117_220a

---
+++ TheWorld/src/TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,24 @@
+﻿// simpleControls.js
+
+//#region Directives - Reusable Control : WaitCursor
+
+    (function () {
+        "use strict";
+
+        angular.module("simpleControls", [])
+            //. provides the ClueNet syntax eg: .Directives
+              .directive("waitCursor", waitCursor);
+        
+        function waitCursor() {
+            return {
+                scope: {
+                    show: "=displayWhen"
+                },
+                restrict: "E",  //restricted to only the element style
+                templateUrl: "/views/waitCursor.html"
+            };
+        }
+
+    })();
+
+//#endregion
